724af943ac46340d5ddb4183387ef65ed730c3fb
crscillitoe <crscillitoe@wpi.edu>
2017-06-14 21:18:09 -0700

    Added explanation of hyperlinks in fuckYouPapachan.py

--- fuckYouPapachan.py
+++ fuckYouPapachan.py
@@ -4,6 +4,11 @@ import urllib3
 #Get the HTML code
 http = urllib3.PoolManager()
 response = http.request('GET' , 'www.puzzle-bridges.com/?size=0')
+
+#size 11 is a 25x25 hard puzzle, which is what we should be importing, but I have size=0
+#uncommented for now since it's a smaller file to read as that board is only 7x7
+#response = http.request('GET' , 'www.puzzle-bridges.com/?size=11')
+
 page_source = response.data
 
 #Make it readable
